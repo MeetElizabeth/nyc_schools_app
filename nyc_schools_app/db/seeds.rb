2adcb0839941dfd6782050a76906a92b742aeefd
MeetElizabeth <ecaberne@gmail.com>
2014-07-14 12:28:06 -0400

    fixed heroku issue, moved require csv elsewhere

--- nyc_schools_app/db/seeds.rb
+++ nyc_schools_app/db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'csv'
 
 Borough.destroy_all
 District.destroy_all
