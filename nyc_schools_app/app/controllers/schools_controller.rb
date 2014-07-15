345691f150db5eb7e727c2c7f46afff22cbfe299
MeetElizabeth <ecaberne@gmail.com>
2014-07-15 19:51:14 -0400

    tweaked some css styling, added footer with my name

--- nyc_schools_app/app/controllers/schools_controller.rb
+++ nyc_schools_app/app/controllers/schools_controller.rb
@@ -1,10 +1,12 @@
+require 'will_paginate/array'
+
 class SchoolsController < ApplicationController
 
   def index
+    @schools = School.all
     @q = School.search(params[:q])
     @schools = @q.result(distinct: true)
   end
-
 
   def show
     @school = School.find(params[:id])
